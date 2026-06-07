046970cfdf71699b934a42f16581dd520dcec620
shaad4 <mohammedshaadn@gmail.com>
2026-06-07 12:02:57 +0530

    feat(asgi): refactor ASGI application to wrap WebSocket routing with JWT authentication middleware

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,26 +1,21 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
+from config.ws_routing import websocket_urlpatterns
+from apps.tenants.ws_middleware import JWTTenantAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django_asgi_app = get_asgi_application()
 
-from config.ws_routing import websocket_urlpatterns
+
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
-        URLRouter(websocket_urlpatterns)
+        JWTTenantAuthMiddleware(
+            URLRouter(websocket_urlpatterns)
+        )
     ),
 })
