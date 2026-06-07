cc894ee1f22fb9f3362ebca8ec2ea32ab81e2f8e
shaad4 <mohammedshaadn@gmail.com>
2026-06-07 12:36:22 +0530

    refactor(asgi): remove unused AllowedHostsOriginValidator from WebSocket routing

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,21 +1,17 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from config.ws_routing import websocket_urlpatterns
-from apps.tenants.ws_middleware import JWTTenantAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django_asgi_app = get_asgi_application()
 
-
+from config.ws_routing import websocket_urlpatterns
+from apps.tenants.ws_middleware import JWTTenantAuthMiddleware
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(
-        JWTTenantAuthMiddleware(
-            URLRouter(websocket_urlpatterns)
-        )
+    "websocket": JWTTenantAuthMiddleware(
+        URLRouter(websocket_urlpatterns)
     ),
 })
--- backend/config/ws_routing.py
+++ backend/config/ws_routing.py
@@ -1,5 +1,3 @@
 from django.urls import path
 
-websocket_urlpatterns = [
-    
-]
+websocket_urlpatterns = []
