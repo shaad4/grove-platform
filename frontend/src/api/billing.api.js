ab4f49f91efe8ea9ee243f8d982265321e5c9ef6
shaad4 <mohammedshaadn@gmail.com>
2026-06-21 12:58:00 +0530

    feat(billing): implement API functions for creating checkout sessions and retrieving billing portal URLs

---
+++ frontend/src/api/billing.api.js
@@ -0,0 +1,17 @@
+import api from './client'
+
+export const createCheckoutSession = ({ successUrl, cancelUrl } = {}) =>
+  api.post('/billing/create-checkout-session/', {
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+  })
+
+export const getBillingPortalUrl = ({ returnUrl } = {}) =>
+api.post('/billing/portal/', {
+return_url: returnUrl,
+})
+
+export const getBillingPortalUrl = ({ returnUrl } = {}) =>
+  api.post('/billing/portal/', {
+    return_url: returnUrl,
+  })
